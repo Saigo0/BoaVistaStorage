ca8b8528a9d8641ded63006b73c4fad5c2c36d65
Jonathan Rezende dos Santos <161469965+DevSanttos@users.noreply.github.com>
2025-06-12 19:55:30 -0300

    Criação do projeto

---
+++ BoaVistaStorage/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.ibdev.boavistastorage {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.ibdev.boavistastorage to javafx.fxml;
+    exports com.ibdev.boavistastorage;
+}
